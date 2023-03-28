56b92a831250e38ed47365a988fe6c202502343c
Gustavo Bacelar <gubacelar@gmail.com>
2023-03-28 14:18:03 -0300

    Fixing to long rest only

--- hooks/delete-item-long-rest.js
+++ hooks/delete-item-long-rest.js
@@ -1,4 +1,4 @@
-Hooks.on('dnd5e.restCompleted', async (actor) => {
+Hooks.on('dnd5e.dnd5e.preLongRest', async (actor) => {
     // Personagem está usando um long rest
     let itemToDeleteName = "Vitality Coconut Water";
     let itemToDelete = actor.items.find(i => i.name === itemToDeleteName);
