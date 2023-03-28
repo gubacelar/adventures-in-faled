ef645a713f8a39dcfdb2edfe1aea1f41c94210d7
Gustavo Bacelar <gubacelar@gmail.com>
2023-03-27 22:39:02 -0300

    Fixing hook and item name

--- hooks/delete-item-long-rest.js
+++ hooks/delete-item-long-rest.js
@@ -1,11 +1,9 @@
-Hooks.on('preUpdateActor', async (actor, updateData, options, userId) => {
-  if (updateData.data.attributes.hp && updateData.data.attributes.hp.value === updateData.data.attributes.hp.max) {
+Hooks.on('dnd5e.restCompleted', async (actor) => {
     // Personagem está usando um long rest
-    let itemToDeleteName = "Nome do item";
+    let itemToDeleteName = "Vitality Coconut Water";
     let itemToDelete = actor.items.find(i => i.name === itemToDeleteName);
     if (itemToDelete) {
       await actor.deleteEmbeddedDocuments("Item", [itemToDelete._id], {deleteAll: false});
-      ui.notifications.notify(`O item "${itemToDeleteName}" foi removido da ficha do personagem "${actor.name}" durante o long rest.`);
     }
-  }
 });
+
